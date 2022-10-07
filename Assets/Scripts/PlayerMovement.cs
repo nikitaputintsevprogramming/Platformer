8748bdac613a3d6d841ebe491263de745f80cd61
NikitaPutintsev <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2022-10-07 18:58:31 +0300

    4. Анимация
    Летучка ≈ 5 мин
    Теория по созданию анимаций ≈ 5 мин
    Создание необходимых анимаций персонажа ≈ 10 мин
    Знакомство с окном анимации ≈ 5 мин
    Знакомство с аниматор контроллером ≈ 10 мин
    Создание дерева переходов анимации персонажа ≈ 10 мин
    Создание и написание скрипта анимации ≈ 10 мин
    Свободное время на вопросы, рефлексия ≈ 5 мин

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+
+
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isGrounded = false;
+    [SerializeField] float speed;
+    [SerializeField] Transform playerCollider;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] float jumpOffset;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        bool isJumping = Input.GetButtonDown("Jump");
+
+        if (isGrounded)
+        {
+            if (isJumping)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+                anim.SetTrigger("IsJump");
+            }
+            else
+            {
+                anim.SetTrigger("IsIdle");
+            }
+        }
+
+
+        if (horizontal != 0)
+        {
+            if (horizontal > 0)
+            {
+                rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
+                spriteRenderer.flipX = true;
+            }
+
+            anim.SetBool("IsRun", true);
+        }
+        else 
+        {
+            anim.SetBool("IsRun", false);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 overlapCircle = playerCollider.position;
+        isGrounded = Physics2D.OverlapCircle(overlapCircle, jumpOffset, groundLayer);
+    }
+}
